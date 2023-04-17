f03c3d48330b46a981c9c5db959c4f88fee6c3e2
Andreas Heinzel <1409997+aheinzel@users.noreply.github.com>
2023-04-17 06:04:36 +0200

    prevent repeated user creation on restart
    allows stopped container to be started again

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,19 @@ user=root
 
 if [ -n "${runas}" ]
 then
-   runas_uid=$(echo "${runas}" | cut -d ":" -f1)
-   runas_gid=$(echo "${runas}" | cut -d ":" -f2)
-   sudo groupadd -g "${runas_gid}" user
-   sudo useradd -d "/home/user" -u "${runas_uid}" -g "${runas_gid}" -s "/bin/bash" user
-   if [ ! -d "/home/user" ]
+   if [ ! -f "/.user_created" ]
    then
-      mkdir "/home/user"
-      chown ${runas_uid}:${runas_gid} "/home/user"
+      runas_uid=$(echo "${runas}" | cut -d ":" -f1)
+      runas_gid=$(echo "${runas}" | cut -d ":" -f2)
+      sudo groupadd -g "${runas_gid}" user
+      sudo useradd -d "/home/user" -u "${runas_uid}" -g "${runas_gid}" -s "/bin/bash" user
+      if [ ! -d "/home/user" ]
+      then
+         mkdir "/home/user"
+         chown ${runas_uid}:${runas_gid} "/home/user"
+      fi
+
+      touch "/.user_created"
    fi
 
    user="user"
